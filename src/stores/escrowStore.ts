16789f03e46e7ee79e6a30e9dfe0708e56b06199
skullnbones <skullandbonesdao@gmail.com>
2024-05-13 18:35:48 +0200

    make sure escrow_selected is not null

--- src/stores/escrowStore.ts
+++ src/stores/escrowStore.ts
@@ -44,6 +44,7 @@ export const useEscrowStore = defineStore('escrowStore', {
       const data = await useWorkspace()?.pg_escrow.value.account.escrow.all();
       console.log(data);
       this.escrows = data as unknown as Array<I_Escrows>;
+      this.escrow_selected = this.escrows[0];
     },
     async load_escrow(address: PublicKey) {
       console.log('load_escrow()');
