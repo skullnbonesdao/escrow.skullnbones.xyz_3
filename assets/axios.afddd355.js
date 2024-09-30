3090bb2a15a71c78bbc132d857059d1efc78c122
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-09-30 00:21:20 +0000

    deploy: 6bb01535d1c818e5ffcb9ffe122a1c99d7610545

--- assets/axios.afddd355.js
+++
@@ -1,1 +0,0 @@
-import{b as i}from"./index.d57d7707.js";import{a}from"./axios.56c8b13d.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
---
+++ assets/axios.b5fd81d6.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.1481476f.js";import{a}from"./axios.4c98d235.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
