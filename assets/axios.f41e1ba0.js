20475a399acbcb1ac54efe8eeee5f5688c34a957
skullnbonesdao <skullnbonesdao@users.noreply.github.com>
2024-11-10 01:18:51 +0000

    deploy: 1adb9c059f28c5e6847a779a1f48242381365a7a

---
+++ assets/axios.f41e1ba0.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.1e72f503.js";import{a}from"./axios.0f6ca1f4.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
